df5d4ed1f57f0c3b78e211022f1f7f79a3a03d2a
MiMo <moritz@newsletter2go.com>
2019-03-15 15:08:02 +0100

    removed sandbox from urls

--- newsletter2go/newsletter2go.php
+++ newsletter2go/newsletter2go.php
@@ -170,7 +170,7 @@ class Newsletter2Go extends Module
                 a.src=c, 
                 i.parentNode.insertBefore(a,i) 
             } 
-            (window,document,"script","//static-sandbox.newsletter2go.com/utils.js","n2g"); 
+            (window,document,"script","//static.newsletter2go.com/utils.js","n2g"); 
             n2g(\'create\', \'' . $companyId . '\'); 
             n2g(\'ecommerce:addTransaction\', ' . json_encode($transactionData) . ');';
 
